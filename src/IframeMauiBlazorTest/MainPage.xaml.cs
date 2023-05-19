1d2ea6fd1b2ee96c1263772220f4221ae0a0ae62
Håkon Nordli <hakon.nordli@xlent.no>
2023-05-19 15:01:02 +0200

    everything

---
+++ src/IframeMauiBlazorTest/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace IframeMauiBlazorTest;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/IframeMauiBlazorTest/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace IframeMauiBlazorTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
